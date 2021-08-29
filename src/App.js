93542ce46085b0bc062a1d60cdfccd35fb169dcc
seungyong88 <gosng2001@naver.com>
2021-08-29 23:19:08 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,53 @@
+console.log("app is running!");
+
+function App($app) {
+  this.state = {}
+
+  const init = () => {
+  }
+
+  init();
+}
+
+export default App;
+
+// class App {
+//   $target = null;
+//   data = [];
+
+//   constructor($target) {
+//     this.$target = $target;
+
+//     this.searchInput = new SearchInput({
+//       $target,
+//       onSearch: keyword => {
+//         api.fetchCats(keyword).then(({ data }) => this.setState(data));
+//       }
+//     });
+
+//     this.searchResult = new SearchResult({
+//       $target,
+//       initialData: this.data,
+//       onClick: image => {
+//         this.imageInfo.setState({
+//           visible: true,
+//           image
+//         });
+//       }
+//     });
+
+//     this.imageInfo = new ImageInfo({
+//       $target,
+//       data: {
+//         visible: false,
+//         image: null
+//       }
+//     });
+//   }
+
+//   setState(nextData) {
+//     console.log(this);
+//     this.data = nextData;
+//     this.searchResult.setState(nextData);
+//   }
+// }
